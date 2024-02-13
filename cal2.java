8c1c6b04cfeb9a5d2555aa29e0c62ce0b5c82c46
01fe22bca225 <156986284+01fe22bca225@users.noreply.github.com>
2024-02-13 10:07:23 +0530

    Update cal2.java

--- cal2.java
+++ cal2.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class cal2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+     //   Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter first number:");
-        double num1 = scanner.nextDouble();
+       // double num1 = scanner.nextDouble();
+        double num1=10;
 
         System.out.println("Enter second number:");
-        double num2 = scanner.nextDouble();
+        //double num2 = scanner.nextDouble();
+        double num2=20;
 
         System.out.println("Choose an operation: +, -, *, /");
-        char operation = scanner.next().charAt(0);
+        //char operation = scanner.next().charAt(0);
+        
 
         double result;
 
